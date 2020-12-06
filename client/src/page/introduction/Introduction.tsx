1c4f456bde6d3e5296a108f3fdaeedc9daf3270c
Koeun Lee <korany@kakao.com>
2020-12-06 16:23:12 +0900

    [Task033] Remove ignored file & Modify App.tsx route

--- client/src/page/introduction/Introduction.tsx
+++ client/src/page/introduction/Introduction.tsx
@@ -1,84 +1,14 @@
 import React from "react";
-import Footer from "../../components/footer/Footer";
-import Header from "../../components/header/Header";
-import styled from "styled-components";
-
-import {
-	mainColor,
-	subColor,
-	inputColor,
-	buttonColor,
-	userColor,
-} from "../../common/colors";
 import IntroBanner from "../../components/introBanner/IntroBanner";
 import GoButton from "../../components/goButton/GoButton";
 
 const Introduction = (): JSX.Element => {
 	return (
-		<Container>
-			<HeaderContainer>
-				<Header />
-			</HeaderContainer>
-			<MainContainer>
-				<IntroBanner />
-				<GoButton />
-			</MainContainer>
-			<FooterContainer>
-				<Footer />
-			</FooterContainer>
-		</Container>
+		<>
+			<IntroBanner />
+			<GoButton />
+		</>
 	);
 };
-
-const Container = styled.div`
-	display: grid;
-	width: 100%;
-	grid-template-rows: auto auto auto;
-	grid-template-columns: 100%;
-	grid-template-areas:
-		"HeaderContainer"
-		"MainContainer"
-		"FooterContainer";
-	@media (max-width: 768px) {
-		grid-template-rows: 1.5em auto auto auto 3em;
-		grid-template-columns: auto 86% auto;
-	}
-`;
-
-const HeaderContainer = styled.div`
-	grid-area: HeaderContainer;
-	width: 100%;
-	margin: 0 0 0 0;
-	@media (max-width: 768px) {
-		margin: 0 0 1em 0;
-	}
-	@media (max-width: 414px) {
-		margin: 0 0 0.4em 0;
-	}
-`;
-
-const MainContainer = styled.div`
-	display: grid;
-	grid-area: MainContainer;
-	background-color: ${mainColor};
-	grid-template-columns: 15em auto;
-	min-height: 600px;
-	margin: 0 0 9em 0;
-	@media (max-width: 768px) {
-		margin: 0 0 4em 0;
-	}
-	@media (max-width: 414px) {
-		margin: 0;
-		min-height: 0;
-	}
-`;
-
-const FooterContainer = styled.div`
-	grid-area: FooterContainer;
-	margin: 0 0 2em 0;
-	@media (max-width: 768px) {
-		display: none;
-	}
-`;
 
 export default Introduction;
--- client/src/styles/global-styles.ts
+++ client/src/styles/global-styles.ts
@@ -6,15 +6,13 @@ export const GlobalStyle = createGlobalStyle`
   body {
     margin: 0;
     padding: 0;
-    @import url('https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,300;0,400;0,700;0,900;1,400&family=Noto+Sans+KR:wght@300;400;500;700&family=Ubuntu:wght@300;400;500;700&display=swap');
-    @import url('https://fonts.googleapis.com/css2?family=Yellowtail&display=swap');
-    font-family: 'Noto Sans KR', sans-serif;
+    @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@500&family=Nanum+Gothic:wght@700&display=swap');
+    font-family: 'Nanum Gothic', sans-serif;
+    /* 영문 'Montserrat', sans-serif; 국문 'Nanum Gothic', sans-serif; */
   }
-
-  * {
+  *{
     box-sizing: border-box;
   }
 
 `;
-
 export const detailTheme = {};
