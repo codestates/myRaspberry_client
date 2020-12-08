c09a53693528a6a967cad2adb2c175ccdbb32dc6
Koeun Lee <korany@kakao.com>
2020-12-09 01:46:11 +0900

    [Task051] Modify intro bottom - resultBox color and border radius

--- client/src/common/colors.ts
+++ client/src/common/colors.ts
@@ -3,6 +3,7 @@ export const headerColor = "#292929";
 export const textColor = "#f5f5f5"; //whitesmoke
 export const subColor = "#E89E9E";
 
+export const pointColor = "#F06363";
 export const inputColor = "#F8E0E1";
 export const buttonColor = "#EC4040";
 export const userColor = "#CF3535";
