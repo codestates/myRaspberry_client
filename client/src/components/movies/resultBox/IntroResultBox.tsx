cd0cf073ec3daf3372c7f53afb5957861b5251ad
Koeun Lee <58801242+korany-lee@users.noreply.github.com>
2020-12-15 23:09:48 +0900

    Revert "[Task078] Modify ResultBox connect to CardDetail"

--- client/src/components/movies/resultBox/IntroResultBox.tsx
+++ client/src/components/movies/resultBox/IntroResultBox.tsx
@@ -10,42 +10,19 @@ import { MoviesType } from "../../../modules/movies";
 
 SwiperCore.use([Navigation]);
 
+interface IntroMovieProps {
+	movies?: any;
+}
+
 const IntroResultBox = () => {
 	const { introMovieState, getIntroMovieData } = useIntroMovies();
 	const [showDetail, setShowDetail] = useState(false);
-	const MOVIE: MoviesType = {
-		id: 0,
-		docid: "",
-		title: "",
-		titleEng: "",
-		director: "",
-		actor: "",
-		plotKr: "",
-		plotEng: "",
-		runtime: 0,
-		genre: "",
-		image: {
-			posters: "",
-			stlls: "",
-		},
-		tag: [],
-		date: "",
-		score: 0,
-	};
-	const [selectMovie, setSelectMovie] = useState<MoviesType>(MOVIE);
-
 	useEffect(() => {
 		getIntroMovieData();
 	}, []);
 
-	const closeMovieDetail = e => {
-		e.preventDefault();
-		document.body.classList.remove("modal-open");
-		setSelectMovie(MOVIE);
-	};
-
 	const { loading, introMovies } = introMovieState;
-	// console.log("무비 한개 들어가야함", selectMovie);
+	console.log("intro", introMovies);
 	return (
 		<>
 			<Swiper
@@ -67,26 +44,11 @@ const IntroResultBox = () => {
 							}
 							style={{ listStyle: "none", width: "100%" }}
 							alt={movie.title}
-							onClick={() => {
-								document.body.classList.add("modal-open");
-								setSelectMovie({ ...movie });
-								setShowDetail(true);
-							}}
+							onClick={() => alert(setShowDetail(true))}
 						/>
 					</SwiperSlide>
 				))}
 			</Swiper>
-			{selectMovie.id ? (
-				<CardDetail
-					poster={
-						selectMovie.image.posters[0] === "image/posters/default.jpg"
-							? "https://i.ibb.co/HnNxZyh/default-poster.jpg"
-							: `https://imgraspberry.s3-accelerate.amazonaws.com/${selectMovie.image.posters[0]}`
-					}
-					movie={selectMovie}
-					closeMovieDetail={closeMovieDetail}
-				/>
-			) : null}
 		</>
 	);
 };
