ce061fe7abe4cb16b60d54fe0118ebd9e2cf71bf
Koeun Lee <58801242+korany-lee@users.noreply.github.com>
2020-12-15 11:54:31 +0900

    Revert "[Task073] Modify network connection with youtube api indirectly"

--- client/src/modules/youtube.ts
+++ client/src/modules/youtube.ts
@@ -99,7 +99,9 @@ export const getVideos = title => async (
 			type: SEARCH_LOADING,
 		});
 
-		const { data } = await axios.get(`https://localhost/movie/${title}`);
+		const { data } = await axios.get(
+			`https://www.googleapis.com/youtube/v3/search?part=snippet&key=${process.env.REACT_APP_YOUTUBE_API_KEY}&q=영화${title}&maxResults=3&type=video&videoEmbeddable=true`
+		);
 		dispatch(searchVideos(data.items));
 	} catch (e) {
 		dispatch(searchFail(e));
