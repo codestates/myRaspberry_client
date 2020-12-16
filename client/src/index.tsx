86612e4ee4de901d3e684c1806c3f70659817500
Ubuntu <ubuntu@my-raspberry.asia-northeast3-a.c.united-yeti-296901.internal>
2020-12-16 09:00:24 +0000

    [No task] Import css file to index.tsx

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -9,6 +9,7 @@ import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
 import rootReducer from "./modules";
 import Thunk from "redux-thunk";
+import "./index.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import { Router } from "react-router-dom";
