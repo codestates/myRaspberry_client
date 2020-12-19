962789e18152d1e8684f82fd89c01e117de41d4d
Koeun Lee <korany@kakao.com>
2020-12-19 21:53:24 +0900

    [Task121] Fix youtube decoded title - add npm he module

--- client/src/components/movies/youtube/Youtube.tsx
+++ client/src/components/movies/youtube/Youtube.tsx
@@ -2,8 +2,11 @@ import React from "react";
 import useYoutube from "../../../hooks/useYoutube";
 import styles from "./youtube.module.css";
 
+const he = require("he");
+
 const Youtube = ({ video, videosKey }) => {
 	const { updateVideos } = useYoutube();
+
 	return (
 		<figure
 			className={styles.videoContainer}
@@ -13,7 +16,9 @@ const Youtube = ({ video, videosKey }) => {
 			}}>
 			<img className={styles.videos} src={video.url} alt="" />
 			<figcaption>
-				<h3 className={styles.title}>{video.title}</h3>
+				<h3 className={styles.title}>
+					{(video.title = he.decode(video.title))}
+				</h3>
 			</figcaption>
 		</figure>
 	);
