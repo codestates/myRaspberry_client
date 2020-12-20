ac4bc6ea5b14aaf5ecf5d1c1084ae779587a44aa
Koeun Lee <korany@kakao.com>
2020-12-20 20:05:46 +0900

    [Task137] Modify IntroBanner

--- client/src/components/introBanner/IntroBanner.tsx
+++ client/src/components/introBanner/IntroBanner.tsx
@@ -10,10 +10,15 @@ const IntroBanner: React.FC = () => {
 			className={close ? styles.show : styles.none}
 			onClick={() => setClose(false)}>
 			<div className={styles.show_icon_box}>
-				<ImPointUp className={styles.show_icon} />
+				<img
+					style={{ height: "14rem", borderRadius: "10px" }}
+					src="https://imgraspberry.s3-accelerate.amazonaws.com/image/intro/introclick002.gif"
+					alt="how to use our web application"
+				/>
 			</div>
 			<div className={styles.show_box}>
 				<span className={styles.show_text}>
+					<ImPointUp className={styles.show_icon} />
 					선호하지 않는 영화를 골라주세요
 				</span>
 				{/* <button className={styles.show_btn} onClick={()=> setClose(false)} >닫기</button> */}
